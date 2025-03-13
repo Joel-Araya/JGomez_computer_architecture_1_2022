f885b8fd1f15606fc59dff51eef11d668047ee78
Joel Gómez <62965696+Joel-Araya@users.noreply.github.com>
2025-03-12 20:45:57 -0600

    Revert "Merge pull request #2 from Joel-Araya/Formato-imagen"
    This reverts commit 16a6a53ad076aa135d8480c49eeda661eba294de, reversing
    changes made to a4f1d29357a12fbba8c873d98d3913e40c9e50dc.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import subprocess
 
-
-def execute_assembly():
-    subprocess.run(["arm-linux-gnueabi-as", "test.s", "-o", "test.o"])
-    subprocess.run(["arm-linux-gnueabi-ld", "test.o", "-o", "test"])
-    subprocess.run(["./test"])
+subprocess.run(["arm-linux-gnueabi-as", "test.s", "-o", "test.o"])
+subprocess.run(["arm-linux-gnueabi-ld", "test.o", "-o", "test"])
 
 
+subprocess.run(["python3", "--version"])
